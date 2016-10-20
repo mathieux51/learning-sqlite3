2dc98a6bb5b7ce07c4ca7df0f6181105881ceb06
mathieux51 <mathieux51@gmail.com>
2016-10-20 18:09:09 +0200

    Implemented db.pets.rad using sqlite3

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const fs = require("fs")
 const file = "pets.db"
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(file);
+const sqldb = new sqlite3.Database(file);
 
-db.serialize(() => {
-    console.log("Inside db.serialize")
-    db.each("select * from pets;", (err, row) => {
-        console.log(row)
-    })
+// db.serialize(() => {
+//     console.log("Inside db.serialize")
+//     db.each("select * from pets;", (err, row) => {
+//         console.log(row)
+//     })
+// })
+
+// sqlite3_db.each("select * from pets;", (err, data) => {
+//     console.log(data)
+//     db.close()
+// })
+
+
+// Database layer
+const db = {
+    pets: {
+        read: function(cb) {
+            sqldb.all("select name from pets;", (err, pets) => {
+                sqldb.close()
+                cb(err, pets)
+            })
+        },
+        save: function(pets, cb) {
+            fs.writeFile('server/list.json', JSON.stringify({ pets }), (err) => {
+                console.log("It's saved!")
+                cb(err)
+            })
+        }
+    }
+}
+
+db.pets.read((err, pets) => {
+    console.log(pets)
 })
-
-db.close()
